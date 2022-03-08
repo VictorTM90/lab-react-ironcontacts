9a96c4fc3523364c8d686b5d141049abbad8e968
victor <victor.tm90@gmail.com>
2022-03-08 18:52:00 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import contacts from "../src/contacts.json"
+import {useState} from "react"
 
 function App() {
+  const [contact, setContact] = useState ( contacts.slice(5,10))
+ 
+ const  handleRandomContact = () =>{
+   const randomNum = Math.floor(Math.random()*contacts.length)
+   const randomContact = contacts[randomNum]
+   setContact( [randomContact, ...contact])
+ }
+  
+ const handleNameContact = () =>{
+   const newContactArr = [...contact]
+   newContactArr.sort((elem1, elem2)=>elem1.name >elem2.name ? 1 :-1 )
+   setContact(newContactArr)
+ }
+ const handlePopularityContact = () =>{
+  const newContactArr = [...contact]
+  newContactArr.sort((elem1, elem2)=> elem1.popularity > elem2.popularity ? -1 :1 )
+  setContact(newContactArr)
+}
+
+const handleDeleteContact = (index) =>{
+  const newContactArr = [...contact]
+  newContactArr.splice(index,1)
+  setContact( newContactArr)
+}
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1>Iron Contacts</h1>
+              <section className='botones'>
+                <button onClick={handleNameContact} className="btn">Order by Name</button>
+                <button onClick={handlePopularityContact} className="btn">Order by popularity</button>
+                <button onClick={handleRandomContact} className="btn">Random Contact</button>
+              </section>
+      <table>
+            <thead>
+            <tr>
+              <th><h3>Foto</h3></th>
+              <th><h3>Name</h3></th>
+              <th><h3>Popularity</h3></th>
+              <th><h3>Won Oscar</h3></th>
+              <th><h3>Won a Emmy</h3></th>
+              <th><h3>Action</h3></th>
+            </tr>
+          </thead>
+          <tbody id="body-bonito">
+            {contact.map((eachContact, index)=>{
+              
+              return (
+              <tr key={ eachContact.id }>
+
+                <td>  <img src={ eachContact.pictureUrl} alt={eachContact.name}  className ="imagen-contact"/></td> 
+                <td>  <h3>{eachContact.name}</h3> </td> 
+                <td>  {eachContact.popularity}  </td>
+                <td>  {eachContact.wonOscar === true && "🏆"}  </td>
+                <td>  {eachContact.wonOscar === true && "🏆"}  </td>
+                <td>  <button  onClick={()=> handleDeleteContact(index)} className="btn">Delete</button>  </td>
+            
+              </tr>  
+            )
+            })
+            }
+          </tbody>
+     </table>
     </div>
   );
 }
